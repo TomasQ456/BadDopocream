396d460a58843e5052fa6dae66bca938da7cb9df
Tomas Quiceno Ostos <tomas.quiceno-o@mail.escuelaing.edu.co>
2025-11-28 07:02:11 -0500

    Delete DopoIce/src directory

--- DopoIce/src/org/junit/jupiter/api/function/Executable.java
+++
@@ -1,9 +0,0 @@
-package org.junit.jupiter.api.function;
-
-/**
- * Functional interface used by {@code assertThrows}.
- */
-@FunctionalInterface
-public interface Executable {
-    void execute() throws Throwable;
-}
